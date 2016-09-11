ce56a30ef8d6eef70d32ddc57dae1390485072b6
Giancarlo Crocetti <gcrocetti@users.noreply.github.com>
2016-09-10 20:44:41 -0400

    Add files via upload

---
+++ ChapterThree/CreateDataFrame.scala
@@ -0,0 +1,34 @@
+package ch03
+
+import java.sql.Date
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by Giancarlo on 9/10/2016.
+  */
+object CreateDataFrame {
+  def main(args:Array[String]){
+    val conf = new SparkConf()
+      .setMaster("local[2]")
+      .setAppName("DataFrames from RDD")
+      .set("spark.executor.memory","2g")
+
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
+
+    val format = new java.text.SimpleDateFormat("dd/MM/yyyy")
+
+    val rdd = sc.textFile("c:/Spark/data/03-IntroductionToSpark/Retail/OnlineRetail.csv")
+      .map(line=>line.split(","))
+      .map(fields=>(fields(0),fields(1),fields(2),fields(3).toInt,new Date(format.parse(fields(4)).getTime()),fields(5).toFloat,fields(6),fields(7)))
+
+    // Providing the names for the columns
+    val df = rdd.toDF("InvoiceNum","Skus","Desc","Qty","Date","uPrice","cID","Country")
+
+    df.printSchema()
+
+
+  }
+}
